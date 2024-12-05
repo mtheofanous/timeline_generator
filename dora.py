d3c117d42fee2145cbe19ee921175a6067e04f0a
Martheo <theofanousmario@gmail.com>
2024-12-05 19:23:23 +0100

    sdas

--- dora.py
+++ dora.py
@@ -148,18 +148,8 @@ def main():
                 st.success("Grid styling options have been reset.")
         if options == "Background":
             st.sidebar.header("Background")
-            selected_option = st.sidebar.selectbox("Select a background option", ["Color", "Image"])
-            if selected_option == "Color":
-                background_color = st.sidebar.color_picker("Pick a color for background", "#FFFFFF")
-                background_image = None
-            elif selected_option == "Image":
-                background_image = st.sidebar.file_uploader("Upload a background image", type=["png", "jpg", "jpeg"])
-                background_color = None
-            if st.sidebar.button("Reset"):
-                background_color = None
-                background_image = None
-                st.success("Background styling options have been reset.")
-            
+            background_color = st.sidebar.color_picker("Pick a color for background", "#FFFFFF")
+            background_image = st.sidebar.file_uploader("Upload a background image", type=["png", "jpg", "jpeg"])
             if st.sidebar.button("Reset"):
                 background_color = None
                 background_image = None
