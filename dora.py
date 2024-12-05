db77be6eec974ef0dabbda12b271c17f7ecd1d1f
Martheo <theofanousmario@gmail.com>
2024-12-05 18:17:04 +0100

    new

--- dora.py
+++ dora.py
@@ -232,24 +232,26 @@ def main():
         st.warning("Please fill in all the required fields.")
         
     
-    buf = io.BytesIO()
-    timeline_obj.write_image(buf, format="png")
-    buf.seek(0)
-    
-    mockup_type = st.selectbox("Select a mockup type", ["story", "post"])
-    
-    mockup_image = simulate_instagram_display(Image.open(buf), mockup_type= mockup_type)
-    st.image(mockup_image, use_container_width=True)
-    
-    # download the image as png
-    # Create a download button
-    st.download_button(
-        label="Download Mockup as PNG",
-        data=buf,
-        file_name=f"instagram_mockup_{mockup_type}.png",
-        mime="image/png"
-    )
+    try:
+        buf = io.BytesIO()
+        timeline_obj.write_image(buf, format="png")
+        buf.seek(0)
         
+        mockup_type = st.selectbox("Select a mockup type", ["story", "post"])
+        
+        mockup_image = simulate_instagram_display(Image.open(buf), mockup_type= mockup_type)
+        st.image(mockup_image, use_container_width=True)
+        
+        # download the image as png
+        # Create a download button
+        st.download_button(
+            label="Download Mockup as PNG",
+            data=buf,
+            file_name=f"instagram_mockup_{mockup_type}.png",
+            mime="image/png"
+        )
+    except:
+        st.warning("Please fill in all the required fields.")
 
     # Display the app
     st.sidebar.markdown(
